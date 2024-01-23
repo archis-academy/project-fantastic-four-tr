88fad20045d28d468c011968dc0252b5526efef9
aykutyildiz26 <105678166+aykutyildiz26@users.noreply.github.com>
2024-01-23 21:11:01 +0300

    [PR-1] - Change Requests

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
+import products from "./products.js";
 const hamburger = document.querySelector(".hamburger");
 const headerNavbarList = document.querySelector(".header-navbar-list");
+const searchInput = document.querySelector(".search-input");
+const searchDropdown = document.querySelector("#search-dropdown");
+const searchDropdownItems = document.querySelectorAll(".search-dropdown-item");
 
 hamburger.addEventListener("click", function () {
   headerNavbarList.classList.toggle("active");
 });
+searchInput.addEventListener("input", (e) => {
+  filterFunction(e);
+});
 
-function filterFunction() {
-  var input, filter, a, i;
-  input = document.querySelector(".search-input");
-  filter = input.value.toUpperCase();
-  div = document.querySelector("#search-dropdown");
-  a = div.getElementsByTagName("a");
-  for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
-    } else {
-      a[i].style.display = "none";
-    }
+function filterFunction(e) {
+  const filter = e.target.value.trim().toUpperCase();
+  searchDropdown.innerHTML = "";
+  if (!filter) {
+    return;
   }
-}
-function toggleSearchDropdown() {
-  var dropdown = document.getElementById("search-dropdown");
-  dropdown.classList.toggle("show");
+
+  const filteredProducts = products.filter((product) =>
+    product.title.toUpperCase().includes(filter)
+  );
+
+  filteredProducts.forEach((filteredProduct) => {
+    const itemLink = document.createElement("a");
+    itemLink.classList.add("search-dropdown-item");
+    itemLink.href = "#";
+    itemLink.textContent = filteredProduct.title;
+    searchDropdown.appendChild(itemLink);
+  });
 }
