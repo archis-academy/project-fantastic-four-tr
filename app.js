93fae260b2038eb132dab0daf329b591fdd80da7
aykutyildiz26 <105678166+aykutyildiz26@users.noreply.github.com>
2024-01-24 17:28:07 +0300

    [PR-1] - Homepage Hero Layout

--- app.js
+++ app.js
@@ -4,6 +4,39 @@ const headerNavbarList = document.querySelector(".header-navbar-list");
 const searchInput = document.querySelector(".search-input");
 const searchDropdown = document.querySelector("#search-dropdown");
 const searchDropdownItems = document.querySelectorAll(".search-dropdown-item");
+const womanDropdownContainer = document.querySelector(
+  ".woman-dropdown-container"
+);
+
+const menDropdownContainer = document.querySelector(".men-dropdown-container");
+console.log(womanDropdownContainer, menDropdownContainer);
+const menClothingProducts = products.filter(
+  (product) => product.category.toLowerCase() === "men's clothing"
+);
+const womenClothingProducts = products.filter(
+  (product) => product.category.toLowerCase() === "women's clothing"
+);
+
+const menDropdownItems = menClothingProducts.map(
+  (product) =>
+    `<a class="dropdown-container-item" href="#">${product.title}</a>`
+);
+const womenDropdownItems = womenClothingProducts.map(
+  (product) =>
+    `<a class="dropdown-container-item" href="#">${product.title}</a>`
+);
+
+if (searchInput && womanDropdownContainer && menDropdownContainer) {
+  menDropdownContainer.innerHTML = menDropdownItems.join("");
+  womanDropdownContainer.innerHTML = womenDropdownItems.join("");
+}
+else{
+  console.error("Missing items but it's okay, just ignore me. ");
+}
+
+womenClothingProducts.forEach((product) => {
+  //console.log(product.title);
+});
 
 hamburger.addEventListener("click", function () {
   headerNavbarList.classList.toggle("active");
