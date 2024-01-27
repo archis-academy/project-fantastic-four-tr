9c3f0605b2f8a8e5b7375f794c716e96fb0797dc
aykutyildiz26 <105678166+aykutyildiz26@users.noreply.github.com>
2024-01-28 02:12:53 +0300

    [PR-1] - API

--- app.js
+++ app.js
@@ -1,47 +1,56 @@
-import products from "./products.js";
 const hamburger = document.querySelector(".hamburger");
 const headerNavbarList = document.querySelector(".header-navbar-list");
 const searchInput = document.querySelector(".search-input");
 const searchDropdown = document.querySelector("#search-dropdown");
 const searchDropdownItems = document.querySelectorAll(".search-dropdown-item");
-const womanDropdownContainer = document.querySelector(
-  ".woman-dropdown-container"
-);
-
+const womanDropdownContainer = document.querySelector(".woman-dropdown-container");
 const menDropdownContainer = document.querySelector(".men-dropdown-container");
-console.log(womanDropdownContainer, menDropdownContainer);
-const menClothingProducts = products.filter(
-  (product) => product.category.toLowerCase() === "men's clothing"
-);
-const womenClothingProducts = products.filter(
-  (product) => product.category.toLowerCase() === "women's clothing"
-);
-
-const menDropdownItems = menClothingProducts.map(
-  (product) =>
-    `<a class="dropdown-container-item" href="#">${product.title}</a>`
-);
-const womenDropdownItems = womenClothingProducts.map(
-  (product) =>
-    `<a class="dropdown-container-item" href="#">${product.title}</a>`
-);
-
-if (searchInput && womanDropdownContainer && menDropdownContainer) {
-  menDropdownContainer.innerHTML = menDropdownItems.join("");
-  womanDropdownContainer.innerHTML = womenDropdownItems.join("");
-} else {
-  console.error("Missing items but it's okay, just ignore me. ");
-}
-
-womenClothingProducts.forEach((product) => {
-  //console.log(product.title);
-});
 
 hamburger.addEventListener("click", function () {
   headerNavbarList.classList.toggle("active");
 });
+
 searchInput.addEventListener("input", (e) => {
   filterFunction(e);
+});
+
+
+function fetchData(url) {
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+}
+
+
+const apiUrl = "https://fakestoreapi.com/products";
+fetchData(apiUrl).then((data) => {
+  const menClothingProducts = data.filter((product) =>
+    product.category.toLowerCase() === "men's clothing"
+  );
+  const womenClothingProducts = data.filter((product) =>
+    product.category.toLowerCase() === "women's clothing"
+  );
+
+  const menDropdownItems = menClothingProducts.map(
+    (product) =>
+      `<a class="dropdown-container-item" href="#">${product.title}</a>`
+  );
+  const womenDropdownItems = womenClothingProducts.map(
+    (product) =>
+      `<a class="dropdown-container-item" href="#">${product.title}</a>`
+  );
+
+  if (searchInput && womanDropdownContainer && menDropdownContainer) {
+    menDropdownContainer.innerHTML = menDropdownItems.join("");
+    womanDropdownContainer.innerHTML = womenDropdownItems.join("");
+  }
 });
 
 function filterFunction(e) {
@@ -51,18 +60,21 @@ function filterFunction(e) {
     return;
   }
 
-  const filteredProducts = products.filter((product) =>
-    product.title.toUpperCase().includes(filter)
-  );
+  fetchData(apiUrl).then((data) => {
+    const filteredProducts = data.filter((product) =>
+      product.title.toUpperCase().includes(filter)
+    );
 
-  filteredProducts.forEach((filteredProduct) => {
-    const itemLink = document.createElement("a");
-    itemLink.classList.add("search-dropdown-item");
-    itemLink.href = "#";
-    itemLink.textContent = filteredProduct.title;
-    searchDropdown.appendChild(itemLink);
+    filteredProducts.forEach((filteredProduct) => {
+      const itemLink = document.createElement("a");
+      itemLink.classList.add("search-dropdown-item");
+      itemLink.href = "#";
+      itemLink.textContent = filteredProduct.title;
+      searchDropdown.appendChild(itemLink);
+    });
   });
 }
+
 
 document.addEventListener("DOMContentLoaded", function () {
   const dots = document.querySelectorAll(".carousel-dot");
