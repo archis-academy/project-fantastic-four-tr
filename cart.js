59b9d1f5c10900726f8657323de044947cb56096
aykutyildiz26 <105678166+aykutyildiz26@users.noreply.github.com>
2024-02-08 16:24:40 +0300

    [PR - 11] - Responsiveness

--- cart.js
+++ cart.js
@@ -1,26 +1,36 @@
 const discountCoupons = [
-  { coupon: "CGMDff8UOU", discount: 0.5, message: "50% discount applied." },
   {
+    id: 1,
+    coupon: "CGMDff8UOU",
+    discount: 0.5,
+    message: "50% discount applied.",
+  },
+  {
+    id: 2,
     coupon: "XgHgDl9zck",
     discount: 0.99,
     message: "99% discount applied... Jezzz",
   },
   {
+    id: 3,
     coupon: "2EDXR86vyE",
     discount: 0.01,
     message: "1% discount applied. It's better than nothing, right?",
   },
   {
+    id: 4,
     coupon: "Pmfl75mngv",
     discount: 0.4,
     message: "40% discount applied.",
   },
   {
+    id: 5,
     coupon: "o675yDul7G",
     discount: 0.7,
     message: "70% discount applied.",
   },
   {
+    id: 6,
     coupon: "iYSaIRZtQs",
     discount: 0.25,
     message: "25% discount applied.",
