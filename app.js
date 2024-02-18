c83651a913534c2ae35eb97a89df88e9708e339f
Alperen Üstün <alperen.ustun39@gmail.com>
2024-02-18 23:03:51 +0300

    [PR-7] Add flash sales wishlist functionality and update styles

--- app.js
+++ app.js
@@ -149,6 +149,13 @@ async function urunleriGetir() {
                       urun.id
                     })" class="add-to-cart">Add To Cart</p>
                     <span class="f-product-discount">-50%</span>
+                    <svg onclick=favoriteFlashSale(${
+                      urun.id
+                    }) id="flashFavoriteIcon${
+        urun.id
+      }" class="flash-sales-wishlist-icon" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M11 7C8.239 7 6 9.216 6 11.95C6 14.157 6.875 19.395 15.488 24.69C15.6423 24.7839 15.8194 24.8335 16 24.8335C16.1806 24.8335 16.3577 24.7839 16.512 24.69C25.125 19.395 26 14.157 26 11.95C26 9.216 23.761 7 21 7C18.239 7 16 10 16 10C16 10 13.761 7 11 7Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
                   </div>
                   <h3 class="f-product-title">${urun.title}</h3>
                   <div class="f-product-price-container">
@@ -167,6 +174,57 @@ async function urunleriGetir() {
                 </div>`;
     })
     .join("");
+
+
+    function renderWishlistHearts() {
+      const wishlistProducts =
+        JSON.parse(localStorage.getItem("wishlistProducts")) || [];
+      wishlistProducts.forEach((product) => {
+        const flashFavoriteIcon = document.getElementById(
+          `flashFavoriteIcon${product.id}`
+        );
+        flashFavoriteIcon.style.fill = "red";
+      });
+    }
+
+    renderWishlistHearts();
+}
+
+function favoriteFlashSale(productId) {
+  const flashFavoriteIcon = document.getElementById(
+    `flashFavoriteIcon${productId}`
+  );
+
+  const wishlistProducts =
+    JSON.parse(localStorage.getItem("wishlistProducts")) || [];
+
+  const wishlistProduct = wishlistProducts.find(
+    (product) => product.id === productId
+  );
+
+  if (!wishlistProduct) {
+    const productToAdd = allProducts.find(
+      (product) => product.id === productId
+    );
+    const newWishlistProducts = [...wishlistProducts, productToAdd];
+    localStorage.setItem(
+      "wishlistProducts",
+      JSON.stringify(newWishlistProducts)
+    );
+    flashFavoriteIcon.style.fill = "red";
+  } else {
+    alert("The product has been removed from the wishlist");
+    removeFromWishlist(productId);
+    flashFavoriteIcon.style.fill = "white";
+  }
+}
+
+function removeFromWishlist(productId) {
+  const wishlistProducts =
+    JSON.parse(localStorage.getItem("wishlistProducts")) || [];
+
+  const newWishlist = wishlistProducts.filter((item) => item.id !== productId);
+  localStorage.setItem("wishlistProducts", JSON.stringify(newWishlist));
 }
 
 urunleriGetir();
